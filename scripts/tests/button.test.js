0ea3ad4b85facd09afa35f5e90148cb8433a6619
Edward Shanahan <edwardshanahan357@gmail.com>
2023-04-01 18:49:10 +0100

    Import all index.html content to button.test.js

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -5,12 +5,25 @@
 const buttonClick = require("../button");
 
 beforeEach(() => {
-  document.body.innerHTML = `<p id="par"></p>`;
+  let fs = require("fs");
+
+  let fileContent = fs.readFileSync("index.html", "utf-8");
+
+  document.open();
+
+  document.write(fileContent);
+
+  document.close();
 });
 
 describe("Test DOM Content", () => {
   test("expects p content to change", () => {
     buttonClick();
     expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
+  });
+
+  test("h1 should exists", () => {
+    buttonClick();
+    expect(document.getElementsByTagName("h1").length).toBe(1);
   });
 });
