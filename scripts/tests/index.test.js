6e10ca1f2f3340a7ebeb63df271c39871e46bd75
Edward Shanahan <edwardshanahan357@gmail.com>
2023-04-01 18:43:16 +0100

    Tested mock DOM using jest

--- scripts/tests/index.test.js
+++
@@ -1,9 +0,0 @@
-const addition = require("../index");
-
-describe("Calculator", () => {
-  describe("Addition Function", () => {
-    test("Should return 42 for 20 + 22", () => {
-      expect(addition(20, 22)).toBe(42);
-    });
-  });
-});
